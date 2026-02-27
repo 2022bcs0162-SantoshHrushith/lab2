a3b3a61117480c59c72c3dabda417d040254c474
SantoshHrushith <hrushith2004@gmail.com>
2026-02-27 23:21:30 +0530

    Model: RF, n_estimators=50, depth=10

--- train.py
+++ train.py
@@ -62,8 +62,8 @@ X_train, X_test, y_train, y_test = train_test_split(
 # ==============================
 modelName = "Random Forest est=100 dep=15"
 model = RandomForestRegressor(
-    n_estimators=100,
-    max_depth=15,
+    n_estimators=50,
+    max_depth=10,
     random_state=42
 )
 
